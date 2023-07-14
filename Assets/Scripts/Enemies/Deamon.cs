49586742c1f05da4f576a5abfed45d8e22a3a4ef
Puszkee <kubalacki1@gmail.com>
2023-07-14 17:36:20 +0200

    b

--- Assets/Scripts/Enemies/Deamon.cs
+++ Assets/Scripts/Enemies/Deamon.cs
@@ -7,26 +7,46 @@ public class Deamon : MonoBehaviour
     private Transform player;
     private bool run = true;
     private Rigidbody rb;
+    private EnemyHealth health;
+    public GameObject balls;
     // Start is called before the first frame update
     void Start()
     {
+        health = GetComponent<EnemyHealth>();
         rb = GetComponent<Rigidbody>();
+        StartCoroutine(ChangeState());
     }
     IEnumerator ChangeState()
     {
         yield return new WaitForSeconds(5);
         run = !run;
+        if (run)
+        {
+            StartCoroutine(Shoot());
+        }
+        else
+            StartCoroutine(ChangeState());
+    }
+    IEnumerator Shoot()
+    {
+        balls.SetActive(true);
+        yield return new WaitForSeconds(3);
+        balls.SetActive(false);
     }
     // Update is called once per frame
     void Update()
     {
+        if(health.health<=0)
+        {
+            Destroy(this.gameObject);
+        }
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        if(Vector3.Distance(transform.position,player.position)>3)
-            transform.LookAt(player);
+        
+        transform.LookAt(player);
         if (run)
         {
-            if(rb.velocity.magnitude<10)
-                rb.AddRelativeForce(new Vector3(0, 0, -10), ForceMode.Impulse);
+            if(rb.velocity.magnitude<5)
+                rb.AddRelativeForce(new Vector3(0, 0, -5), ForceMode.Impulse);
         }
         else
         {
